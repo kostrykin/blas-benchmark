f8669e8f89ebba87f24891cc55054fd5e7019aba
Leonid Kostrykin <void@evoid.de>
2024-02-23 17:02:24 +0000

    Fix

--- runscript_template.sh
+++ runscript_template.sh
@@ -3,10 +3,10 @@ eval "$$(conda shell.bash hook)"
 
 export CONDA_PREFIX="${prefix}/${config_id}"
 
-conda env create -f "results/${config_id}/environment.yml" --prefix "$CONDA_PREFIX"
+conda env create -f "results/${config_id}/environment.yml" --prefix "$$CONDA_PREFIX"
 
-conda activate "$CONDA_PREFIX"
+conda activate "$$CONDA_PREFIX"
 
 python -m benchmark.cli ${args} --config "${config_id}"
 
-rm -rf "$CONDA_PREFIX"
+rm -rf "$$CONDA_PREFIX"
